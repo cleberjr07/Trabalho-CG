8e131addaae043aec1131590d9c823d9bd333e43
Cleber Farias Berndsen Junior <cleberjr123@gmail.com>
2023-03-30 12:51:12 -0300

    arrumando gls

--- gl2.js
+++ gl2.js
@@ -5,22 +5,18 @@
  const ATTR_NORMAL_LOC		= 1;
  const ATTR_UV_NAME			= "a_uv";
  const ATTR_UV_LOC			= 2;
-
- //--------------------------------------------------
- // Util  Class 
- //--------------------------------------------------
+ 
  class GlUtil{
 
- 	//Convert Hex colors to float arrays, can batch process a list into one big array.
- 	//example : GlUtil.rgbArray("#FF0000","00FF00","#0000FF");
+ 	//Converte cores hexadecimais em arrays flutuantes
  	static rgbArray(){
  		if(arguments.length == 0) return null;
  		var rtn = [];
 
  		for(var i=0,c,p; i < arguments.length; i++){
  			if(arguments[i].length < 6) continue;
- 			c = arguments[i];		//Just an alias(copy really) of the color text, make code smaller.
- 			p = (c[0] == "#")?1:0;	//Determine starting position in char array to start pulling from
+ 			c = arguments[i];		
+ 			p = (c[0] == "#")?1:0;	//Determine a posição inicial no array char para começar a puxar
 
  			rtn.push(
  				parseInt(c[p]	+c[p+1],16)	/ 255.0,
@@ -33,38 +29,29 @@
 
  }
 
-
- //--------------------------------------------------
- // Custom GL Context Object
- //--------------------------------------------------
  function GLInstance(canvasID){
  	var canvas = document.getElementById(canvasID),
  		gl = canvas.getContext("webgl2");
 
  	if(!gl){ console.error("WebGL context is not available."); return null; }
 
- 	//...................................................
- 	//Setup custom properties
- 	gl.mMeshCache = [];	//Cache all the mesh structs, easy to unload buffers if they all exist in one place.
+ 	//Configura as propriedades
+ 	gl.mMeshCache = [];	//Armazena todos os objetos mesh em um cache para facilitar a descarga dos buffers se todos estiverem em um só lugar.
  	gl.mTextureCache = [];
 
- 	//...................................................
- 	//Setup GL, Set all the default configurations we need.
- 	gl.cullFace(gl.BACK);								//Back is also default
- 	gl.frontFace(gl.CCW);								//Dont really need to set it, its ccw by default.
- 	gl.enable(gl.DEPTH_TEST);							//Shouldn't use this, use something else to add depth detection
- 	gl.enable(gl.CULL_FACE);							//Cull back face, so only show triangles that are created clockwise
- 	gl.depthFunc(gl.LEQUAL);							//Near things obscure far things
- 	gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);	//Setup default alpha blending
- 	gl.clearColor(1.0,1.0,1.0,1.0);	//Set clear color
-
- 	//...................................................
- 	//Methods
-
- 	//Reset the canvas with our set background color.	
+ 	//Configura o WebGL, definindo todas as configurações padrão necessárias.
+ 	gl.cullFace(gl.BACK);	//Define qual face será recortada, BACK é o padrão
+	gl.frontFace(gl.CCW);	//Define a ordem de vértices usada para definir as faces, CCW (Counter-ClockWise) é o padrão
+	gl.enable(gl.DEPTH_TEST);	//Habilita o teste de profundidade
+	gl.enable(gl.CULL_FACE);	//Habilita o recorte de faces traseiras, ou seja, só mostra triângulos criados no sentido horário
+	gl.depthFunc(gl.LEQUAL);	//Configura a função de teste de profundidade como menor ou igual (LEQUAL), o que significa que objetos mais próximos vão obscurecer os objetos mais distantes
+	gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);	//Configura o modo de mesclagem alpha padrão
+	gl.clearColor(1.0,1.0,1.0,1.0);	//Define a cor de fundo
+ 	
+	//Limpa o canvas com a cor de fundo definida anteriormente.
  	gl.fClear = function(){ this.clear(this.COLOR_BUFFER_BIT | this.DEPTH_BUFFER_BIT); return this; }
 
- 	//Create and fill our Array buffer.
+ 	//Cria e preenche um buffer de arrays
  	gl.fCreateArrayBuffer = function(floatAry,isStatic){
  		if(isStatic === undefined) isStatic = true; //So we can call this function without setting isStatic
 
@@ -75,30 +62,28 @@
  		return buf;
  	}
 
- 	//Turns arrays into GL buffers, then setup a VAO that will predefine the buffers to standard shader attributes.
+	//Transforma arrays em buffers GL e define um VAO que pré-define os buffers para atributos do shader padrão.
  	gl.fCreateMeshVAO = function(name,aryInd,aryVert,aryNorm,aryUV,vertLen){
  		var rtn = { drawMode:this.TRIANGLES };
 
- 		//Create and bind vao
+ 		//Creia e define vao
  		rtn.vao = this.createVertexArray();															
- 		this.bindVertexArray(rtn.vao);	//Bind it so all the calls to vertexAttribPointer/enableVertexAttribArray is saved to the vao.
+ 		this.bindVertexArray(rtn.vao);	//Define o vao para que todas as chamadas de vertexAttribPointer/enableVertexAttribArray sejam salvas no vao.
 
- 		//.......................................................
- 		//Set up vertices
+ 		
+ 		//Configura os vértices
  		if(aryVert !== undefined && aryVert != null){
- 			rtn.bufVertices = this.createBuffer();													//Create buffer...
- 			rtn.vertexComponentLen = vertLen || 3;													//How many floats make up a vertex
- 			rtn.vertexCount = aryVert.length / rtn.vertexComponentLen;								//How many vertices in the array
+ 			rtn.bufVertices = this.createBuffer();													//Cria um buffer
+ 			rtn.vertexComponentLen = vertLen || 3;													//Quantos flutuadores formam um vértice
+ 			rtn.vertexCount = aryVert.length / rtn.vertexComponentLen;								//Quantos vértices na matriz
 
  			this.bindBuffer(this.ARRAY_BUFFER, rtn.bufVertices);
- 			this.bufferData(this.ARRAY_BUFFER, new Float32Array(aryVert), this.STATIC_DRAW);		//then push array into it.
- 			this.enableVertexAttribArray(ATTR_POSITION_LOC);										//Enable Attribute location
- 			//this.vertexAttribPointer(ATTR_POSITION_LOC,3,this.FLOAT,false,0,0);						//Put buffer at location of the vao\
- 			this.vertexAttribPointer(ATTR_POSITION_LOC,rtn.vertexComponentLen,this.FLOAT,false,0,0);						//Put buffer at location of the vao
+ 			this.bufferData(this.ARRAY_BUFFER, new Float32Array(aryVert), this.STATIC_DRAW);		
+ 			this.enableVertexAttribArray(ATTR_POSITION_LOC);										//Habilita localização do atributo
+ 			this.vertexAttribPointer(ATTR_POSITION_LOC,rtn.vertexComponentLen,this.FLOAT,false,0,0);//Coloca o buffer no local do vao
  		}
 
- 		//.......................................................
- 		//Setup normals
+ 		//Configura normas
  		if(aryNorm !== undefined && aryNorm != null){
  			rtn.bufNormals = this.createBuffer();
  			this.bindBuffer(this.ARRAY_BUFFER, rtn.bufNormals);
@@ -107,29 +92,26 @@
  			this.vertexAttribPointer(ATTR_NORMAL_LOC,3,this.FLOAT,false, 0,0);
  		}
 
- 		//.......................................................
- 		//Setup UV
+ 		//Configura UV
  		if(aryUV !== undefined && aryUV != null){
  			rtn.bufUV = this.createBuffer();
  			this.bindBuffer(this.ARRAY_BUFFER, rtn.bufUV);
  			this.bufferData(this.ARRAY_BUFFER, new Float32Array(aryUV), this.STATIC_DRAW);
  			this.enableVertexAttribArray(ATTR_UV_LOC);
- 			this.vertexAttribPointer(ATTR_UV_LOC,2,this.FLOAT,false,0,0);	//UV only has two floats per component
+ 			this.vertexAttribPointer(ATTR_UV_LOC,2,this.FLOAT,false,0,0);	//UV tem apenas dois floats por componente
  		}
 
- 		//.......................................................
- 		//Setup Index.
+ 		//Configura Index.
  		if(aryInd !== undefined && aryInd != null){
  			rtn.bufIndex = this.createBuffer();
  			rtn.indexCount = aryInd.length;
  			this.bindBuffer(this.ELEMENT_ARRAY_BUFFER, rtn.bufIndex);  
  			this.bufferData(this.ELEMENT_ARRAY_BUFFER, new Uint16Array(aryInd), this.STATIC_DRAW);
- 			//this.bindBuffer(this.ELEMENT_ARRAY_BUFFER,null); //TODO REMOVE THIS AND ADD TO CLEANUP
  		}
 
- 		//Clean up
- 		this.bindVertexArray(null);					//Unbind the VAO, very Important. always unbind when your done using one.
- 		this.bindBuffer(this.ARRAY_BUFFER,null);	//Unbind any buffers that might be set
+ 		//Limpa o buffer
+ 		this.bindVertexArray(null);
+ 		this.bindBuffer(this.ARRAY_BUFFER,null);
  		if(aryInd != null && aryInd !== undefined)  this.bindBuffer(this.ELEMENT_ARRAY_BUFFER,null);
 
  		this.mMeshCache[name] = rtn;
@@ -144,15 +126,15 @@
 
  	gl.fUpdateTexture = function(name,img,doYFlip,noMips){
  		var tex = this.mTextureCache[name];	
- 		if(doYFlip == true) this.pixelStorei(this.UNPACK_FLIP_Y_WEBGL, true);	//Flip the texture by the Y Position, So 0,0 is bottom left corner.
+ 		if(doYFlip == true) this.pixelStorei(this.UNPACK_FLIP_Y_WEBGL, true);
 
- 		this.bindTexture(this.TEXTURE_2D, tex);														//Set text buffer for work
- 		this.texImage2D(this.TEXTURE_2D, 0, this.RGBA, this.RGBA, this.UNSIGNED_BYTE, img);			//Push image to GPU.
+ 		this.bindTexture(this.TEXTURE_2D, tex);														//Definir buffer de texto para rodar
+ 		this.texImage2D(this.TEXTURE_2D, 0, this.RGBA, this.RGBA, this.UNSIGNED_BYTE, img);			//Bota a img na CPU
 
  		if(noMips === undefined || noMips == false){
- 			this.texParameteri(this.TEXTURE_2D, this.TEXTURE_MAG_FILTER, this.LINEAR);					//Setup up scaling
- 			this.texParameteri(this.TEXTURE_2D, this.TEXTURE_MIN_FILTER, this.LINEAR_MIPMAP_NEAREST);	//Setup down scaling
- 			this.generateMipmap(this.TEXTURE_2D);	//Precalc different sizes of texture for better quality rendering.
+ 			this.texParameteri(this.TEXTURE_2D, this.TEXTURE_MAG_FILTER, this.LINEAR);					//Configura up scaling
+ 			this.texParameteri(this.TEXTURE_2D, this.TEXTURE_MIN_FILTER, this.LINEAR_MIPMAP_NEAREST);	//Configura down scaling
+ 			this.generateMipmap(this.TEXTURE_2D);	//Pré-calcula diferentes tamanhos de textura para renderização de melhor qualidade.
  		}else{
  			this.texParameteri(this.TEXTURE_2D, this.TEXTURE_MAG_FILTER, this.NEAREST);
  			this.texParameteri(this.TEXTURE_2D, this.TEXTURE_MIN_FILTER, this.NEAREST);
@@ -160,62 +142,46 @@
  			this.texParameteri(this.TEXTURE_2D, this.TEXTURE_WRAP_T, this.CLAMP_TO_EDGE);
  		}
 
- 		this.bindTexture(this.TEXTURE_2D,null);									//Unbind
+ 		this.bindTexture(this.TEXTURE_2D,null);
 
- 		if(doYFlip == true) this.pixelStorei(this.UNPACK_FLIP_Y_WEBGL, false);	//Stop flipping textures
+ 		if(doYFlip == true) this.pixelStorei(this.UNPACK_FLIP_Y_WEBGL, false);	//Para de inverter texturas
  		return tex;	
  	}
 
- 	//imgAry must be 6 elements long and images placed in the right order
- 	//RIGHT,LEFT,TOP,BOTTOM,BACK,FRONT
  	gl.fLoadCubeMap = function(name,imgAry){
  		if(imgAry.length != 6) return null;
-
- 		//Cube Constants values increment, so easy to start with right and just add 1 in a loop
- 		//To make the code easier costs by making the imgAry coming into the function to have
- 		//the images sorted in the same way the constants are set.
- 		//	TEXTURE_CUBE_MAP_POSITIVE_X - Right	:: TEXTURE_CUBE_MAP_NEGATIVE_X - Left
- 		//	TEXTURE_CUBE_MAP_POSITIVE_Y - Top 	:: TEXTURE_CUBE_MAP_NEGATIVE_Y - Bottom
- 		//	TEXTURE_CUBE_MAP_POSITIVE_Z - Back	:: TEXTURE_CUBE_MAP_NEGATIVE_Z - Front
-
  		var tex = this.createTexture();
  		this.bindTexture(this.TEXTURE_CUBE_MAP,tex);
 
- 		//push image to specific spot in the cube map.
+ 		//Bora a imagem para um ponto específico no mapa do cubo.
  		for(var i=0; i < 6; i++){
  			this.texImage2D(this.TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, this.RGBA, this.RGBA, this.UNSIGNED_BYTE, imgAry[i]);
  		}
 
- 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_MAG_FILTER, this.LINEAR);	//Setup up scaling
- 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_MIN_FILTER, this.LINEAR);	//Setup down scaling
- 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_WRAP_S, this.CLAMP_TO_EDGE);	//Stretch image to X position
- 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_WRAP_T, this.CLAMP_TO_EDGE);	//Stretch image to Y position
- 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_WRAP_R, this.CLAMP_TO_EDGE);	//Stretch image to Z position
- 		//this.generateMipmap(this.TEXTURE_CUBE_MAP);
+ 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_MAG_FILTER, this.LINEAR);	//Configura up scaling
+ 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_MIN_FILTER, this.LINEAR);	//Configura down scaling
+ 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_WRAP_S, this.CLAMP_TO_EDGE);	//Estica imagem para posição X
+ 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_WRAP_T, this.CLAMP_TO_EDGE);	//Estica imagem para posição Y
+ 		this.texParameteri(this.TEXTURE_CUBE_MAP, this.TEXTURE_WRAP_R, this.CLAMP_TO_EDGE);	//Estica imagem para posição Z
 
  		this.bindTexture(this.TEXTURE_CUBE_MAP,null);
  		this.mTextureCache[name] = tex;
  		return tex;
  	};
 
- 	//...................................................
- 	//Setters - Getters
+ 	//Setters/Getters
 
- 	//Set the size of the canvas html element and the rendering view port
+ 	//Defini o tamanho do canvas html e o view port de renderização
  	gl.fSetSize = function(w,h){
- 		//set the size of the canvas, on chrome we need to set it 3 ways to make it work perfectly.
  		this.canvas.style.width = w + "px";
  		this.canvas.style.height = h + "px";
  		this.canvas.width = w;
  		this.canvas.height = h;
-
- 		//when updating the canvas size, must reset the viewport of the canvas 
- 		//else the resolution webgl renders at will not change
  		this.viewport(0,0,w,h);
  		return this;
  	}
 
- 	//Set the size of the canvas to fill a % of the total screen.
+ 	//Define o tamanho do canvas para preencher uma % da tela total.
  	gl.fFitScreen = function(wp,hp){ return this.fSetSize(window.innerWidth * (wp || 1),window.innerHeight * (hp || 1)); }
 
  	return gl;
