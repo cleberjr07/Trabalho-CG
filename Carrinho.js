4c7d69d3c5096fb75653e71c1dfbb87b1fdaff47
Cleber Farias Berndsen Junior <cleberjr123@gmail.com>
2023-04-02 23:05:16 -0300

    finalizando

--- Carrinho.js
+++ Carrinho.js
@@ -1,64 +1,81 @@
-  const addcart = (imagem, nome, preco) => {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
-    cart.push({ imagem, nome, preco });
-    localStorage.setItem("cart", JSON.stringify(cart));
-  };
+// função para adicionar um item ao carrinho
+const addcart = (item) => {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push(item);
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
 
-  const displayCart = () => {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+const displayCart = () => {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-    const cartItems = document.getElementById("cart-items");
+  const cartItems = document.getElementById("cart-items");
+  if (cartItems) {
+  cartItems.innerHTML = "";
+  
 
-    cartItems.innerHTML = "";
+  let total = 0;
 
-    let total = 0;
+  cart.forEach((item, index) => {
+    const cartItem = document.createElement("tr");
 
-    cart.forEach((item, index) => {
-      const cartItem = document.createElement("tr");
+    cartItem.innerHTML = `
+      <td><img src="${item.imagem}" alt="${item.nome}" width="100"></td>
+      <td>${item.nome}</td>
+      <td>R$ ${item.preco.toFixed(2)}</td>
+      <td><button onclick="removeFromCart(${index})">Remover do carrinho</button></td>
+    `;
 
-      cartItem.innerHTML = `
-        <td><img src="${item.imagem}" alt="${item.nome}" width="100"></td>
-        <td>${item.nome}</td>
-        <td>R$ ${item.preco.toFixed(2)}</td>
-        <td><button onclick="removeFromCart(${index})">Remover do carrinho</button></td>
-      `;
+    cartItems.appendChild(cartItem);
 
-      cartItems.appendChild(cartItem);
+    total += item.preco;
+  });
 
-      total += item.preco;
-    });
+  const totalPrice = document.getElementById("totalPrice");
+  totalPrice.textContent = `R$ ${total.toFixed(2)}`;
+}};
 
-    const totalPrice = document.getElementById("totalPrice");
-    totalPrice.textContent = `R$ ${total.toFixed(2)}`;
-  };
+// função para remover um item do carrinho
+const removeFromCart = (index) => {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-  const removeFromCart = (index) => {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.splice(index, 1);
 
-    cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
 
-    localStorage.setItem("cart", JSON.stringify(cart));
+  displayCart();
+};
 
-    displayCart();
-  };
-
-  const clearButton = document.getElementById("clearButton");
-
+// função para limpar o carrinho
+const clearButton = document.getElementById("clearButton");
+if (clearButton) {
   clearButton.addEventListener("click", () => {
     localStorage.removeItem("cart");
     displayCart();
   });
+}
 
-  const addToCartButtons = document.querySelectorAll(".add-to-cart");
 
-  addToCartButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const itemImage = button.parentElement.querySelector(".item-image").getAttribute("src");
-      const itemName = button.parentElement.querySelector(".item-name").textContent;
-      const itemPrice = button.parentElement.querySelector(".item-price").textContent;
-      addcart(itemImage, itemName, itemPrice);
-      displayCart();
-    });
-  });
+const queryString = window.location.search;
+if (queryString) {
+  const urlParams = new URLSearchParams(queryString);
+  const itemImage = urlParams.get("imagem");
+  const itemName = urlParams.get("nome");
+  const itemPrice = urlParams.get("preco");
 
+  addcart({ imagem: itemImage, nome: itemName, preco: parseFloat(itemPrice) });
   displayCart();
+}
+
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+addToCartButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const itemImage = button.parentElement.querySelector(".item-image").getAttribute("src");
+    const itemName = button.parentElement.querySelector(".item-name").textContent;
+    const itemPrice = button.parentElement.querySelector(".item-price").textContent;
+    addcart({ imagem: itemImage, nome: itemName, preco: parseFloat(itemPrice) });
+    displayCart();
+  });
+});
+
+// exibir os itens do carrinho
+displayCart();
